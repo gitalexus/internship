51fc703cd28af236a2132203b7975c541ad54dd3
gitalexus <ru9.alex@gmail.com>
2024-01-21 23:57:05 +0300

    feat: add sticky menu behavior

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+import './utils/focus-lock';
+import './utils/scroll-lock';
 import { initMenu } from './modules/menu';
 import { initSliders } from './modules/slider';
 
--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,15 +1,53 @@
 function initMenu() {
   const menuBtn = document.querySelector('[data-menu-button]');
-  const menu = document.querySelector('[data-menu]');
   const menuList = document.querySelector('[data-menu-list]');
+  const menuOverlay = document.querySelector('[data-menu="overlay"]');
 
   setTimeout(() => {
     menuList.classList.remove('visually-hidden');
   }, 500);
 
-  menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('is-active');
-  });
+  menuBtn.addEventListener('click', changeMenuState);
+  menuOverlay.addEventListener('click', changeMenuState);
+  document.addEventListener('scroll', onScrollMenuActions);
+}
+
+function onEscPress(evt) {
+  if (evt.key === 'Escape') {
+    changeMenuState();
+  }
+}
+
+function changeMenuState() {
+  const menu = document.querySelector('[data-menu]');
+
+  menu.classList.toggle('is-active');
+  if (menu.classList.contains('is-active')) {
+    window.focusLock.lock('.menu__container');
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('keydown', onEscPress);
+  } else {
+    window.focusLock.unlock('.menu__container');
+    document.body.style.overflow = '';
+    document.removeEventListener('keydown', onEscPress);
+  }
+}
+
+const menuBlock = document.querySelector('[data-header="sticky-menu"]');
+const MENU_TOP_OFFSET = 31;
+const MENU_HIDDEN_OFFSET = -55; //+ 31;
+let menuPos = MENU_TOP_OFFSET;
+let scrollY = 0;
+
+function onScrollMenuActions() {
+  const newScrollY = window.scrollY;
+  const delta = newScrollY - scrollY;
+  scrollY = newScrollY;
+  menuPos = (delta > 0)
+    ? MENU_HIDDEN_OFFSET
+    : MENU_TOP_OFFSET;
+
+  menuBlock.style.top = `${menuPos}px`;
 }
 
 export { initMenu };
