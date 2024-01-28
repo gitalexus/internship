48212a9e64ca626e0008320f9bad6d0009c0c331
gitalexus <ru9.alex@gmail.com>
2024-01-28 22:03:40 +0300

    feat: add programs block

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -2,6 +2,8 @@
 import Swiper from 'swiper/bundle';
 
 const swiperHero = document.querySelector('[data-swiper="hero"]');
+const swiperPrograms = document.querySelector('[data-swiper="programs"]');
+
 const swiperHeroOptions = {
   uniqueNavElements: false,
   pagination: {
@@ -17,6 +19,37 @@ const swiperHeroOptions = {
   loop: true,
 };
 
+const swiperProgramsOptions = {
+  speed: 750,
+  grabCursor: true,
+  navigation: {
+    nextEl: '[data-swiper-button="programs-next"]',
+    prevEl: '[data-swiper-button="programs-prev"]',
+  },
+  scrollbar: {
+    el: '[data-swiper-scrollbar="programs"]',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    420: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+    720: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+    1330: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+      allowTouchMove: false,
+      grabCursor: false,
+    },
+  },
+};
+
 function initSlider(slierElement, sliderOptions) {
   return slierElement
     ? new Swiper(slierElement, sliderOptions)
@@ -25,6 +58,7 @@ function initSlider(slierElement, sliderOptions) {
 
 function initSliders() {
   initSlider(swiperHero, swiperHeroOptions);
+  initSlider(swiperPrograms, swiperProgramsOptions);
 }
 
 export {initSliders};
